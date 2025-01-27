7cb85b65ce4a3eb19eb8913562768439531506b5
Ansar <ansarshayekin1@gmail.com>
2025-01-27 14:00:31 +0000

    refactoring 1.0

--- script.js
+++ script.js
@@ -243,6 +243,62 @@ class StartScreen {
     }
 }
 
+class Timer {
+    startTime;
+    passedTime = 0;
+    timeInterval;
+    paused = false;
+    running = false;
+    object;
+
+    constructor(object) {
+        this.object = object;
+    }
+
+    renderTimer() {
+        const minutes = (this.passedTime.getUTCMinutes()).toString().padStart(2, '0');
+        const seconds = (this.passedTime.getUTCSeconds()).toString().padStart(2, '0');
+        this.object.textContent = `${minutes}:${seconds}`;
+    }
+
+    startTimer() {
+        if (this.running) {
+            console.log("Таймер уже работает");
+            return;
+        }
+        this.running = true;
+        this.paused = false;
+
+        this.timeInterval = setInterval(() => {
+            const currentTime = Date.now();
+            this.passedTime = new Date(currentTime - this.startTime);
+            this.renderTimer();
+        }, 1000);
+        this.startTime = Date.now() - this.passedTime;
+    }
+
+    pauseTimer() {
+        this.paused = true;
+        this.running = false;
+        clearInterval(this.timeInterval);
+    }
+
+    stopTimer() {
+        this.paused = false;
+        this.running = false;
+        this.resetTimer();
+        clearInterval(this.timeInterval);
+    }
+
+    resetTimer(resetView = false) {
+        this.startTime = Date.now();
+        this.passedTime = new Date(0);
+        if (resetView) {
+            this.renderTimer();
+        }
+    }
+}
+
 
 // const appContainer = document.getElementById("screen_main");
 // const gameBoard = new GameBoard(appContainer, 15);
